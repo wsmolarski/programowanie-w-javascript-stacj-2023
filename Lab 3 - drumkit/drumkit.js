2b1af24cde950ed3934a31ce4b5c76095df67536
wsmolarski <wmsmolarski@gmail.com>
2023-12-20 20:21:42 +0100

    lab 3 finished

--- Lab 3 - drumkit/drumkit.js
+++ Lab 3 - drumkit/drumkit.js
@@ -2,14 +2,69 @@ document.addEventListener('keypress', onKeyPress)
 
 const KeyToSound = {
     'a': document.querySelector('#s1'),
-    's': document.querySelector('#s2')
+    's': document.querySelector('#s2'),
+    'd': document.querySelector('#s3'),
+    'f': document.querySelector('#s4'),
+    'g': document.querySelector('#s5'),
+    'h': document.querySelector('#s6'),
+    'j': document.querySelector('#s7'),
+    'k': document.querySelector('#s8'),
+    'l': document.querySelector('#s9'),
+}
+
+const channels = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+}
+
+let currentChannel = 1
+
+const savedChannels = JSON.parse(sessionStorage.getItem("channels"))
+
+if (savedChannels) {
+    Object.keys(savedChannels).forEach(channel=>{
+        channels[channel]=savedChannels[channel]
+    })
 }
 
 function onKeyPress(event) {
-    const sound = KeyToSound[event.key]
-    playSound(sound)
+    const pressedKey = event.key.toLowerCase()
+    const sound = KeyToSound[pressedKey]
+    if (sound) {
+        playSound(sound)
+        recordSound(pressedKey, sound.src)
+    } else if(pressedKey == "r") {
+        currentChannel = (currentChannel % 4) + 1
+    } else if(pressedKey == "p") {
+        playRecordedSounds(currentChannel)
+    } else if (pressedKey == "o") {
+        playRecordedSoundsAll()
+    }
 }
 function playSound(sound) {
     sound.currentTime = 0
     sound.play()
+}
+
+function recordSound(key, soundSrc) {
+    channels[currentChannel].push({key, soundSrc})
+    saveChannelsToSessionStorage()
+}
+
+function saveChannelsToSessionStorage() {
+    sessionStorage.setItem(channels, JSON.stringify(channels))
+}
+
+function playRecordedSounds(channel) {
+    channels[channel].forEach(({soundSrc}, index) => {setTimeout(()=>{
+        const sound = new Audio(soundSrc)
+        playSound(sound)
+        },100 * index)
+    })
+}
+
+function playRecordedSoundsAll() {
+    Object.keys(channels).forEach(channel=>{playRecordedSounds(parseInt(channel))})
 }
